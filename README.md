05c5039ef4a53f159ede9e3c5ceb686d76fbd5a2
guillaumervls <3765057+guillaumervls@users.noreply.github.com>
2025-02-22 15:25:31 +0100

    connect preview status

--- README.md
+++ README.md
@@ -1,36 +1,5 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# Neon Onboarding Template
 
-## Getting Started
+This is the template repo for a demo app that Neon offers to spawn for its new users, to take a tour of the branching workflow that Neon enables.
 
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+It's meant to be deployed to Vercel with GitHub Actions.
--- app/page.tsx
+++ app/page.tsx
@@ -25,6 +25,13 @@ export default async function Home() {
     .select()
     .from(todosTable)
     .orderBy(todosTable.created_at);
+
+  const isPreview = !!process.env.NEXT_PUBLIC_GITHUB_PULL_NUMBER;
+
+  const codeLocation = isPreview
+    ? `github.com/${process.env.NEXT_PUBLIC_GITHUB_REPOSITORY}/tree/${process.env.NEXT_PUBLIC_GITHUB_REF}`
+    : `github.com/${process.env.NEXT_PUBLIC_GITHUB_REPOSITORY}`;
+
   return (
     <div className="min-h-screen flex flex-col gap-8 p-4 md:py-12 justify-center max-w-screen-md mx-auto">
       <div className="self-center">
@@ -33,8 +40,8 @@ export default async function Home() {
           {PAGE_TITLE}
         </div>
         <div className="my-2 font-bold">
-          This is a fully functional todo app hosted on Vercel backed by your
-          Neon database.
+          This is your own fully functional todo app hosted on Vercel backed by
+          your Neon database.
         </div>
         <div className="my-2 text-sm">
           It has a GitHub Actions CI/CD setup to automatically deploy the
@@ -44,19 +51,30 @@ export default async function Home() {
         <div className="my-2 text-sm">
           The code is here:{" "}
           <a
-            href={`https://github.com/${process.env.NEXT_PUBLIC_GITHUB_REPOSITORY}/tree/${process.env.NEXT_PUBLIC_GITHUB_REF}`}
+            href={`https://${codeLocation}`}
             target="_blank"
             className="font-mono text-xs underline text-green-600"
           >
-            github.com/{process.env.NEXT_PUBLIC_GITHUB_REPOSITORY}
+            {codeLocation}
           </a>
         </div>
         <div className="my-2">
           — <br />
           This is{" "}
-          {process.env.NEXT_PUBLIC_GITHUB_REF === "main"
-            ? 'the "production" version'
-            : 'a "preview" version'}
+          {isPreview ? (
+            <span>
+              a &quot;preview&quot; version for{" "}
+              <a
+                href={`https://github.com/${process.env.NEXT_PUBLIC_GITHUB_REPOSITORY}/pull/${process.env.NEXT_PUBLIC_GITHUB_PULL_NUMBER}`}
+                target="_blank"
+                className="underline"
+              >
+                pull request #{process.env.NEXT_PUBLIC_GITHUB_PULL_NUMBER}
+              </a>
+            </span>
+          ) : (
+            'the "production" version'
+          )}
           . It uses:
           <ul className="ml-2 mt-2 list-disc list-inside text-sm leading-loose">
             <li>
@@ -80,40 +98,71 @@ export default async function Home() {
           — <br />
           <div>Now, your move:</div>
           <ol className="ml-6 mt-2 list-decimal list-outside text-sm">
-            <li className="mb-2">
-              Add some to-dos first — you&apos;ll see how they&apos;ll also
-              appear in your preview deployment. This is because of the
-              branching capabilities Neon databases have.
-            </li>
+            {isPreview ? (
+              <>
+                <li className="mb-2">
+                  See that the title ({PAGE_TITLE}) has changed. But the to-dos
+                  below are the same that were in production. This is because
+                  this preview branch of your database was created as a fork of
+                  production.
+                </li>
+                <li className="mb-2">
+                  Add some more todos below (or remove some), this will be saved
+                  in the preview branch of the database.
+                  <em> But not in production.</em> (You can check for yourself.)
+                </li>
+              </>
+            ) : (
+              <li className="mb-2">
+                Add some to-dos first — you&apos;ll see how they&apos;ll also
+                appear in your preview deployment. This is because of the
+                branching capabilities Neon databases have.
+              </li>
+            )}
             <li>
-              Use this box to create a pull request editing the title of this
-              page:
+              {isPreview
+                ? "Use this button to merge the pull request and deploy to production:"
+                : "Use this box to create a pull request editing the title of this page:"}
               <form
-                action={addTodo}
+                action={`${process.env.NEXT_PUBLIC_NEON_ONBOARDING_ORIGIN}/onboarding/${process.env.NEXT_PUBLIC_NEON_ONBOARDING_ID}/pulls`}
+                method="post"
                 className="inline-flex gap-2 items-baseline ml-2 leading-normal"
               >
                 <input
-                  placeholder={PAGE_TITLE}
-                  required
-                  type="text"
-                  name="title"
-                  id="title"
-                  className="bg-inherit text-inherit rounded border border-gray-500/50 focus:border-foreground outline-none px-2 py-1 flex-1"
+                  type="hidden"
+                  name="action"
+                  value={isPreview ? "merge" : "open"}
                 />
+                {isPreview ? (
+                  <input
+                    type="hidden"
+                    name="pullNumber"
+                    value={process.env.NEXT_PUBLIC_GITHUB_PULL_NUMBER}
+                  />
+                ) : (
+                  <input
+                    placeholder={PAGE_TITLE}
+                    required
+                    type="text"
+                    name="newPageTitle"
+                    className="bg-inherit text-inherit rounded border border-gray-500/50 focus:border-foreground outline-none px-2 py-1 flex-1"
+                  />
+                )}
+
                 <button
                   type="submit"
                   className="font-bold rounded bg-gray-400/40 px-2 py-1"
                 >
-                  Open PR
+                  {isPreview ? "Merge" : "Open"} PR
                 </button>
                 <br />
               </form>
             </li>
             <div className="mt-2 leading-tight opacity-50">
               <em className="text-xs">
-                We need you to use this box because the repo is still under our
-                GitHub org and GitHub Actions enforces limitations for security
-                reasons on &quot;foreign&quot; pull-requests workflows.
+                We need you to use this button because the repo is still under
+                our GitHub org and GitHub Actions enforces limitations for
+                security reasons on &quot;foreign&quot; pull-requests workflows.
               </em>
             </div>
           </ol>
